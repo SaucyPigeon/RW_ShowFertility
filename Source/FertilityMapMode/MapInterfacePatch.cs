22203c225987e6efd9b2dc8d35d0274c00209a18
SaucyPigeon <36158286+SaucyPigeon@users.noreply.github.com>
2020-02-18 23:17:26 +0000

    Initial #2 (source code)

---
+++ Source/FertilityMapMode/MapInterfacePatch.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Harmony;
+using RimWorld;
+using RimWorld.Planet;
+using Verse;
+
+namespace FertilityMapMode
+{
+	[HarmonyPatch(typeof(MapInterface))]
+	[HarmonyPatch("MapInterfaceOnGUI_BeforeMainTabs")]
+	public class MapInterfacePatch
+	{
+		public static void Postfix()
+		{
+			if (!WorldRendererUtility.WorldRenderedNow)
+			{
+				FertilityDrawer.FertilityDrawerOnGUI();
+			}
+		}
+	}
+}
